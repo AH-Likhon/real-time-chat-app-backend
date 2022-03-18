f0884f786f23b6a928b03200fbd28023699ed547
AH-Likhon <ictianlikhon6@gmail.com>
2022-03-19 00:12:18 +0600

    success and error alert

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.use(cookieParser());
 async function run() {
     try {
         await client.connect();
-        const database = client.db('New-Messenger');
+        const database = client.db('Messenger');
         const users = database.collection('users');
+
+
+        // get a user
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await users.findOne(query);
+            res.json(user)
+        })
 
         // insert user
         app.post('/users', async (req, res) => {
@@ -34,44 +43,54 @@ async function run() {
                 image
             } = req.body;
 
-            const error = [];
+            let error = "";
 
-            if (!userName) {
-                error.push('please provide your user name');
-                // res.json({ error });
+            if ( !userName || !email || !password || !confirmPassword || !image ) {
+                error = "Fill up the input fields!";
+                // res.json({ error })
             }
-            if (!email) {
-                error.push('please provide your email');
+            if (!userName && email && password && confirmPassword && image) {
+                // error.push('please provide your user name');
+                error = 'please provide your user name';
+            }
+            if (userName && !email && password && confirmPassword && image) {
+                error = 'please provide your email';
             }
             if (email && !validator.isEmail(email)) {
-                error.push('please provide your valid email');
+                error = 'please provide your valid email';
+                // res.json({ error })
             }
-            if (!password) {
-                error.push('please provide your password');
+            if ( userName && email && !password && confirmPassword && image ) {
+                error = 'please provide your password';
+                // res.json({ error })
             }
-            if (!confirmPassword) {
-                error.push('please provide user confirm password');
+            if ( userName && email && password && !confirmPassword && image) {
+                error = 'please provide user confirm password';
+                // res.json({ error })
             }
             if (password && confirmPassword && password !== confirmPassword) {
-                error.push('your password and confirm password not same');
+                error = 'your password and confirm password not same';
+                // res.json({ error })
             }
             if (password && password.length < 6) {
-                error.push('please provide password must be 6 charecter');
+                error = 'please provide password must be 6 charecter';
+                // res.json({ error })
             }
-            if (!image) {
-                error.push('please provide user image');
+            if ( userName && email && password && confirmPassword && !image ) {
+                error = 'please provide your image';
+                // res.json({ error })
             }
-            if (error.length > 0) {
-                res.json({ error })
+            
+            if(error){
+                res.json({ error });
             } else {
-
                     
                 try {
                     const checkUser = await users.findOne({ email: email });
                     console.log(checkUser);
 
                     if(checkUser){
-                        error.push("Your email is already registered");
+                        error = "Your email is already registered";
                         res.json({ error });
                     }else{
                         const user = await users.insertOne({
@@ -86,14 +105,26 @@ async function run() {
         
 
                         // console.log(token);
+
+                        // const result = user;
+                        console.log(user);
+
+                        // const token = jwt.sign({
+                        //         id: user._id,
+                        //         email: user.email,
+                        //         userName: user.userName,
+                        //         image: user.image,
+                        //         registerTime: user.registerTime
+                        //     }, process.env.SECRET, {
+                        //         expiresIn: process.env.TOKEN_EXP
+                        //     });
+
+                        // res.send(token);
                         const options = {
                             expires : new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000)
                         }
 
-                        const result = user;
-                        console.log(result);
-
-                        res.cookie('authToken', result, options).json(user)
+                        res.cookie('authToken', user, options).json(user);
 
                         // res.json(token);
                     }
@@ -102,7 +133,7 @@ async function run() {
                 } catch (error) {
                     res.status(500).json({
                                     error: {
-                                        errorMessage: ['Internal server error']
+                                        errorMessage: 'Internal server error'
                                     }
                                 })
                 }               
