d17e294aa94d340d81338d0461a3a82b47e90df8
plcpl-haque <md.haque@profecialinks.com>
2022-06-26 23:07:12 +0600

    url update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const app = express();
 const dotenv = require('dotenv');
 // const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
@@ -13,13 +13,26 @@ const { MongoClient } = require('mongodb');
 const { authMiddleware } = require('./middleware/authMiddleware');
 const PORT = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3g5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// `mongodb://[${process.env.DB_USER}:${process.env.DB_PASS}@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]`
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3g5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3g5t.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb://[process.env.DB_USER:process.env.DB_PASS@]`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
+// app.use(cors({
+//     origin: ["http://localhost:3000", "http://localhost:5000"],
+//     // credentials: true,
+// }));
+// app.use(express.json());
+// app.use(bodyParser.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 
 async function run() {
@@ -202,7 +215,7 @@ async function run() {
                                 expires: new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000)
                             }
 
-                            res.cookie('authToken', token, options).json({
+                            res.status(201).cookie('authToken', token, options).json({
                                 successMessage: 'Successfully login',
                                 token
                             });
