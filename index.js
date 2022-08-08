1f3cf22063d1afee51ce20603d05d853f3a08fa6
plcpl-haque <md.haque@profecialinks.com>
2022-08-08 11:32:35 +0600

    again header set

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-app.use(cookieParser());
-
 app.use((req, res, next) => {
     res.set({
         "Access-Control-Allow-Origin": "*",
@@ -31,6 +23,15 @@ app.use((req, res, next) => {
     });
     next();
 });
+
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(cookieParser());
+
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http, {
