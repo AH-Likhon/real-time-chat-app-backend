bde78eda10982fd69136db6603ccd4da812e1f2e
plcpl-haque <md.haque@profecialinks.com>
2022-08-08 13:04:25 +0600

    again options

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors({
-    origin: 'https://fierce-bastion-47070.herokuapp.com',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
 }));
 
