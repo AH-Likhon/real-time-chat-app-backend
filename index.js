34c33a4d744cd37bf0659575f75ad3b1480edf36
plcpl-haque <md.haque@profecialinks.com>
2022-08-08 13:46:33 +0600

    cors with live site

--- index.js
+++ index.js
@@ -15,14 +15,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// app.use(cors({
-//     origin: 'http://localhost:3000/',
-//     optionsSuccessStatus: 200
-// }));
-
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://stalwart-unicorn-e6d085.netlify.app',
     credentials: true,
     optionSuccessStatus: 200
 }
